7b8667683a6169564ed2e8680b9c25c75f5083e8
nikhilkumar2002 <nikunikhil0409@gmail.com>
2025-03-06 15:41:10 +0530

    [Nikhil] Added DTO

--- src/main/java/com/example/EmployeePayroll/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeePayroll/controller/EmployeeController.java
@@ -1,13 +1,14 @@
 package com.example.EmployeePayroll.controller;
 
+import com.example.EmployeePayroll.dto.EmployeeDTO;
 import com.example.EmployeePayroll.model.Employee;
-import com.example.EmployeePayroll.repository.EmployeeRepository;
 import com.example.EmployeePayroll.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employeepayrollservice")
@@ -16,33 +17,75 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
-
     // GET All Employees
     @GetMapping
-    public List<Employee> getAllEmployees() {
-        return employeeService.getAllEmployees();
+    public List<EmployeeDTO> getAllEmployees() {
+        return employeeService.getAllEmployees()
+                .stream()
+                .map(emp -> new EmployeeDTO(
+                        emp.getName(),
+                        emp.getGender(),
+                        emp.getDepartment(),
+                        emp.getSalary(),
+                        emp.getStartDate()
+                ))
+                .collect(Collectors.toList());
     }
 
     // GET Employee by ID
     @GetMapping("/get/{id}")
-    public Employee getEmployeeById(@PathVariable Long id) {
-        return employeeService.getEmployeeById(id);
+    public EmployeeDTO getEmployeeById(@PathVariable Long id) {
+        Employee emp = employeeService.getEmployeeById(id);
+        return new EmployeeDTO(
+                emp.getName(),
+                emp.getGender(),
+                emp.getDepartment(),
+                emp.getSalary(),
+                emp.getStartDate()
+        );
     }
 
     // POST Create New Employee
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee addEmployee(@RequestBody Employee employee) {
-        return employeeService.addEmployee(employee);
+    public EmployeeDTO addEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        Employee emp = new Employee(
+                null,
+                employeeDTO.getName(),
+                employeeDTO.getGender(),
+                employeeDTO.getDepartment(),
+                employeeDTO.getSalary(),
+                employeeDTO.getStartDate()
+        );
+        Employee savedEmp = employeeService.addEmployee(emp);
+        return new EmployeeDTO(
+                savedEmp.getName(),
+                savedEmp.getGender(),
+                savedEmp.getDepartment(),
+                savedEmp.getSalary(),
+                savedEmp.getStartDate()
+        );
     }
 
     // PUT Update Employee
     @PutMapping("/update/{id}")
-    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee) {
-        updatedEmployee.setId(id);
-        return employeeService.updateEmployee(id, updatedEmployee);
+    public EmployeeDTO updateEmployee(@PathVariable Long id, @RequestBody EmployeeDTO employeeDTO) {
+        Employee emp = new Employee(
+                id,
+                employeeDTO.getName(),
+                employeeDTO.getGender(),
+                employeeDTO.getDepartment(),
+                employeeDTO.getSalary(),
+                employeeDTO.getStartDate()
+        );
+        Employee updatedEmp = employeeService.updateEmployee(id, emp);
+        return new EmployeeDTO(
+                updatedEmp.getName(),
+                updatedEmp.getGender(),
+                updatedEmp.getDepartment(),
+                updatedEmp.getSalary(),
+                updatedEmp.getStartDate()
+        );
     }
 
     // DELETE Employee
---
+++ src/main/java/com/example/EmployeePayroll/dto/EmployeeDTO.java
@@ -0,0 +1,16 @@
+package com.example.EmployeePayroll.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDTO {
+    private String name;
+    private String gender;
+    private String department;
+    private double salary;
+    private String startDate;
+}
