8f0abf3bd339a8d1539819475a836c32d415ad58
nikhilkumar2002 <nikunikhil0409@gmail.com>
2025-03-06 15:13:44 +0530

    [Nikhil] Added MySql

--- src/main/java/com/example/EmployeePayroll/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeePayroll/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.EmployeePayroll.controller;
 
 import com.example.EmployeePayroll.model.Employee;
+import com.example.EmployeePayroll.repository.EmployeeRepository;
 import com.example.EmployeePayroll.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,26 +16,36 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // GET All Employees
     @GetMapping
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
+    // GET Employee by ID
     @GetMapping("/get/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
         return employeeService.getEmployeeById(id);
     }
 
+    // POST Create New Employee
     @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
     public Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
     }
 
-    @PutMapping("/update")
-    public Employee updateEmployee(@RequestBody Employee updatedEmployee) {
-        return employeeService.updateEmployee(updatedEmployee.getId(), updatedEmployee);
+    // PUT Update Employee
+    @PutMapping("/update/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee) {
+        updatedEmployee.setId(id);
+        return employeeService.updateEmployee(id, updatedEmployee);
     }
 
+    // DELETE Employee
     @DeleteMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable Long id) {
--- src/main/java/com/example/EmployeePayroll/model/Employee.java
+++ src/main/java/com/example/EmployeePayroll/model/Employee.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Table(name="employee")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
